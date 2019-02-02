b956bc3cff1055fe4e1be1a0c527f3e612e20a3d
Esdras Bezerra da Silva <bzr4@icloud.com>
2019-02-02 20:44:00 -0200

    Criado Operação de Instanciação de objetos com a interface CommandLineRunner

--- src/main/java/br/com/logicsp/learning/SpringLearningUdemyApplication.java
+++ src/main/java/br/com/logicsp/learning/SpringLearningUdemyApplication.java
@@ -1,13 +1,31 @@
 package br.com.logicsp.learning;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.logicsp.learning.domain.Categoria;
+import br.com.logicsp.learning.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringLearningUdemyApplication {
+public class SpringLearningUdemyApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearningUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(0, "Informática");
+		Categoria categoria2 = new Categoria(0, "Escritório");
+		
+		repository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
