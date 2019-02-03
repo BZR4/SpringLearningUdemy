90b31e295f0226c6f47f7f4bbcfee96f252c818f
Esdras Bezerra da Silva <bzr4@icloud.com>
2019-02-02 23:12:37 -0200

    Correção de referencia ciclica com anotacoes @JsonManagedReference e @JsonBackReference nos relacionamentos

--- src/main/java/br/com/logicsp/learning/domain/Categoria.java
+++ src/main/java/br/com/logicsp/learning/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 	/**
@@ -26,6 +28,7 @@ public class Categoria implements Serializable {
 	 * A Categoria pode ter muitos produtos associados 
 	 */
 
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	List<Produto> produtos = new ArrayList<>();
 	
